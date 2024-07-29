d44e5ca9f84cd90955211228579247f5997d21af
Andy Kwiatkowski <andy@strata.ltd>
2024-07-29 13:52:18 -0400

    converting to tyhpescript

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,50 @@
-let allRoads = [];
-let allMeters = [];
-let parkingBlocks = {};
+import * as L from 'leaflet';
+
+interface Node {
+  id: number;
+  lat: number;
+  lon: number;
+}
+
+interface Way {
+  nodes: number[];
+  tags: {
+    highway?: string;
+  };
+}
+
+interface OverpassResponse {
+  elements: (Node | Way | any)[];
+}
+
+let allRoads: [Vec, Vec][] = [];
+let allMeters: { coords: [number, number]; gfx: L.Circle }[] = [];
+let parkingBlocks: { [key: string]: L.Polygon } = {};
+
 const starting_lat = 43.53255820104378; // hard-coded for now
 const starting_lng = -80.23672699928285;
 
-const bbox = [
-  // the bounding box for which we're going to download road data from OSM
+const bbox: [number, number][] = [
   [43.543523, -80.225332],
   [43.511111, -80.250074],
 ];
 
-// some math constants for converting to vector coords, Latitude: 1 deg = 110.574 km. Longitude: 1 deg = 111.320*cos(latitude) km.
 const latkm = 110.574;
 const lngkm = 111.32 * Math.cos((starting_lat * Math.PI) / 180);
 const conversionFactor = lngkm / latkm;
 
-// create the map using the Leaflet library
-const map = L.map("map").setView([starting_lat, starting_lng], 17);
+const map = L.map("map", { attributionControl: false }).setView([starting_lat, starting_lng], 17);
 const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 99,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// button handler for clicking on map to add a meter
 map.on("click", onMapClick);
+document.getElementById("make-blocks-button")?.addEventListener("click", onMakeBlocksButton_Clicked);
 
-// button handler for making the parking blocks
-document
-  .getElementById("make-blocks-button")
-  .addEventListener("click", onMakeBlocksButton_Clicked);
-
-// download all the road data in the bounding box, and draw the road lines and nodes
 fetchDataInBoundingBox(bbox)
   .then((data) => {
-    data.nodes.forEach((coord) => {
+    data.nodes.forEach((coord: Node) => {
       L.circle([coord.lat, coord.lon], {
         color: "#ff7800",
         weight: 1,
@@ -42,27 +52,23 @@ fetchDataInBoundingBox(bbox)
       }).addTo(map);
     });
 
-    data.ways.forEach((way) => {
-      let coords = way.nodes.map((node) => {
-        const coord = data.nodes.find((coord) => coord.id === node);
+    data.ways.forEach((way: Way) => {
+      let coords = way.nodes.map((nodeId) => {
+        const coord = data.nodes.find((coord: Node) => coord.id === nodeId);
         if (!coord) {
-          //   console.log(`Could not find coord for node ${node}`);
-          return [null, null];
+          return [-1, -1] as [number, number];
         }
-        return [coord.lat, coord.lon];
+        return [coord.lat, coord.lon] as [number, number];
       });
-      // filter out all coords with null values
-      coords = coords.filter((coord) => coord[0] !== null);
 
-      // draw the way (road)
+      coords = coords.filter((coord) => coord[0] !== null) as [number, number][];
+
       if (coords.length > 1 && way.tags.highway) {
-        // push every line segment into allRoads for use late when making blocks
         for (let i = 0; i < coords.length - 1; i++) {
           allRoads.push([
             convertMapToVec(coords[i]),
             convertMapToVec(coords[i + 1]),
           ]);
-          // draw the line segment
           L.polyline([coords[i], coords[i + 1]], {
             color: "#00ff00",
             weight: 1,
@@ -73,7 +79,7 @@ fetchDataInBoundingBox(bbox)
   })
   .catch((error) => console.error(error));
 
-function onMapClick(e) {
+function onMapClick(e: L.LeafletMouseEvent) {
   let c = L.circle([e.latlng.lat, e.latlng.lng], {
     color: "green",
     weight: 10,
@@ -82,8 +88,8 @@ function onMapClick(e) {
 
   allMeters.push({ coords: [e.latlng.lat, e.latlng.lng], gfx: c });
 }
+
 function onMakeBlocksButton_Clicked() {
-  // clear all blocks
   for (let key in parkingBlocks) {
     map.removeLayer(parkingBlocks[key]);
   }
@@ -94,10 +100,9 @@ function onMakeBlocksButton_Clicked() {
   });
 }
 
-function addBlockForMeter(lat, lng) {
+function addBlockForMeter(lat: number, lng: number) {
   let meterLocation = convertMapToVec([lat, lng]);
 
-  // find closest road to the meter
   let closestRoad = allRoads.reduce((prev, curr) => {
     const prevDist = distancePointToLineSegment(meterLocation, prev);
     const currDist = distancePointToLineSegment(meterLocation, curr);
@@ -108,34 +113,27 @@ function addBlockForMeter(lat, lng) {
   let roadDirection = vecSubtract(closestRoad[1], closestRoad[0]);
   roadDirection = vecNormalize(roadDirection);
 
-  // get normal vector, this is the direction perpendicular to the road
-  let normal = vecNormalize({
-    x: -roadDirection.y,
-    y: roadDirection.x,
-  });
+  let normal = vecNormalize({ x: -roadDirection.y, y: roadDirection.x });
 
-  // determine which side of the road the meter is on using dot product
-  let diff = vecSubtract(meterLocation, closestRoad[0]); // vector from a point on the road to the meter
+  let diff = vecSubtract(meterLocation, closestRoad[0]);
   let dot = vecDot(diff, normal);
   let reversed = dot < 0;
   if (reversed) {
     normal = vecScale(normal, -1);
   }
 
-  // check if this block already exists, if it does let's not add it again
   let key = `${closestRoad[0].x},${closestRoad[0].y},${closestRoad[1].x}, ${closestRoad[1].y} :${reversed}`;
   if (key in parkingBlocks) {
     return;
   }
 
-  const blockSize = 10 / 111139; // 10 meters in degrees (roughly, this is a hack)
+  const blockSize = 10 / 111139;
 
-  // block starts at the meter and goes blockSize meters in the normal direction (away from the road)
   let start = vecAdd(
-    closestRoad[0], // start of the road
+    closestRoad[0],
     vecAdd(
-      vecScale(normal, distanceToRoad), // offset from the road, so the block won't draw on the road
-      vecScale(roadDirection, distanceToRoad) // offset down the street, so the block won't draw on intersecting streets
+      vecScale(normal, distanceToRoad),
+      vecScale(roadDirection, distanceToRoad)
     )
   );
   let start2 = vecAdd(start, vecScale(normal, blockSize));
@@ -148,7 +146,6 @@ function addBlockForMeter(lat, lng) {
   );
   let end2 = vecAdd(end, vecScale(normal, blockSize));
 
-  // draw the block
   const polygon = L.polygon(
     [
       convertVecToMap(start),
@@ -167,35 +164,29 @@ function addBlockForMeter(lat, lng) {
   parkingBlocks[key] = polygon;
 }
 
-// Retrieve data in a bounding box using the Overpass API
-function fetchDataInBoundingBox(bbox) {
-  bbox.sort((a, b) => a[0] - b[0]); // sort by lat, needed for overpass API
+function fetchDataInBoundingBox(bbox: [number, number][]) {
+  bbox.sort((a, b) => a[0] - b[0]);
   const url = `https://overpass-api.de/api/interpreter?data=[out:json];(way["highway"](${bbox});>;);out;`;
 
   return fetchWithCaching(url).then((data) => {
-    // filter out all nodes
-    let returnData = {};
-    returnData.nodes = data.elements.filter(
-      (element) => element.type === "node"
-    );
-    // filter out all ways
-    returnData.ways = data.elements.filter((element) => element.type === "way");
-    // filter out all relations
-    returnData.relations = data.elements.filter(
-      (element) => element.type === "relation"
-    );
+    let returnData: { nodes: Node[]; ways: Way[]; relations: any[] } = {
+      nodes: data.elements.filter((element: any) => element.type === "node"),
+      ways: data.elements.filter((element: any) => element.type === "way"),
+      relations: data.elements.filter(
+        (element: any) => element.type === "relation"
+      ),
+    };
 
     return returnData;
   });
 }
-function fetchWithCaching(url) {
-  // Check if the response is already in the cache
-  if ("caches" in window) {
+
+function fetchWithCaching(url: string) {
+  if ("caches" in window && false) {
     return caches.match(url).then((cachedResponse) => {
       if (cachedResponse) {
         return cachedResponse.json();
       } else {
-        // If the response is not in the cache, fetch it and add it to the cache
         return fetch(url).then((response) => {
           const clonedResponse = response.clone();
           caches.open("osm-cache").then((cache) => {
@@ -206,16 +197,14 @@ function fetchWithCaching(url) {
       }
     });
   } else {
-    // If the browser does not support the Cache API, fetch the response normally
     return fetch(url).then((response) => response.json());
   }
 }
 
-function distancePointToLineSegment(point, segment) {
+function distancePointToLineSegment(point: Vec, segment: [Vec, Vec]): number {
   const [p1, p2] = segment;
 
   const diff = vecSubtract(p2, p1);
-
   const dot = vecDot(diff, vecSubtract(point, p1));
 
   const lenSq = diff.x * diff.x + diff.y * diff.y;
@@ -237,40 +226,46 @@ function distancePointToLineSegment(point, segment) {
   return vecLength(diff2);
 }
 
-// substracts two vectors
-function vecSubtract(v1, v2) {
+interface Vec {
+  x: number;
+  y: number;
+}
+
+function vecSubtract(v1: Vec, v2: Vec): Vec {
   return { x: v1.x - v2.x, y: v1.y - v2.y };
 }
 
-function vecAdd(v1, v2) {
+function vecAdd(v1: Vec, v2: Vec): Vec {
   return { x: v1.x + v2.x, y: v1.y + v2.y };
 }
-function vecNormalize(v) {
+
+function vecNormalize(v: Vec): Vec {
   const len = Math.sqrt(v.x * v.x + v.y * v.y);
   return { x: v.x / len, y: v.y / len };
 }
 
-function arrayToVec(arr) {
+function arrayToVec(arr: [number, number]): Vec {
+
   return { x: arr[0], y: arr[1] };
 }
 
-// dot product of two vectors
-function vecDot(v1, v2) {
+function vecDot(v1: Vec, v2: Vec): number {
   return v1.x * v2.x + v1.y * v2.y;
 }
 
-function vecLength(v) {
+function vecLength(v: Vec): number {
   return Math.sqrt(v.x * v.x + v.y * v.y);
 }
 
-function vecScale(v, s) {
+function vecScale(v: Vec, s: number): Vec {
   return { x: v.x * s, y: v.y * s };
 }
 
-function convertMapToVec([lat, lng]) {
+function convertMapToVec([lat, lng]: [number, number]): Vec {
   return { x: lat, y: lng * conversionFactor };
 }
 
-function convertVecToMap({ x, y }) {
+function convertVecToMap({ x, y }: Vec): [number, number] {
   return [x, y / conversionFactor];
 }
+
